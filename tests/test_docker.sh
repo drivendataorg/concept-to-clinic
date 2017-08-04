565e1b0296e4e5462b74b7e3b4b550df2c5b7ae5
Isaac Slavitt <isaac.slavitt@gmail.com>
2017-08-04 15:48:41 -0400

    Fail build when any test fails

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -6,10 +6,10 @@
 # sh tests/test_docker.sh
 
 # run the model service's tests
-docker-compose -f local.yml run prediction pytest
+docker-compose -f local.yml run prediction pytest || { exit 1; }
 
 # run the project's tests
-docker-compose -f local.yml run interface python manage.py test
+docker-compose -f local.yml run interface python manage.py test || { exit 1; }
 
 # return non-zero status code if there are migrations that have not been created
 docker-compose -f local.yml run interface python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; exit 1; }
