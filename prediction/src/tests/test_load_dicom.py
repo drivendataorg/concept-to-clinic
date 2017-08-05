66ee43e095424a3413e3146538fbfdf6f52d51e8
tdraebing <thomas.draebing@gmail.com>
2017-08-05 15:01:51 +0200

    Implemented function to load a dicom series into memory as a numpy-array
    Add image importing wireframe
    
    Make test images available in local.yml
    
    Changed test-file path to new location and deleted the duplicated test files
    
    Slightly improved error handling
    
    Linting with Flake8

---
+++ prediction/src/tests/test_load_dicom.py
@@ -0,0 +1,47 @@
+import dicom
+import dicom_numpy
+import numpy as np
+import os
+import pytest
+
+from ..preprocess import load_dicom as ld
+from ..preprocess import errors
+
+
+@pytest.fixture
+def dicom_path():
+    return '../images/LIDC-IDRI-0001/1.3.6.1.4.1.14519.5.2.1.6279.6001.298806137288633453246975630178/' \
+           '1.3.6.1.4.1.14519.5.2.1.6279.6001.179049373636438705059720603192'
+
+
+def test_read_files(dicom_path):
+    files = ld._read_files(os.path.join(dicom_path, '*.dcm'))
+
+    assert isinstance(files, list)
+    assert len(files) > 0
+    assert isinstance(files[0], dicom.dataset.Dataset)
+
+    with pytest.raises(dicom.errors.InvalidDicomError):
+        ld._read_files(os.path.join(dicom_path, '*.xml'))
+
+    with pytest.raises(errors.EmptyDicomSeriesException):
+        ld._read_files(os.path.join('./', '*.dcm'))
+
+
+def test_extract_voxel_data(dicom_path):
+    files = ld._read_files(os.path.join(dicom_path, '*.dcm'))
+    dicom_array = ld._extract_voxel_data(files)
+
+    assert isinstance(dicom_array, np.ndarray)
+    assert dicom_array.shape[2] == len(files)
+
+    with pytest.raises(dicom_numpy.DicomImportException):
+        ld._extract_voxel_data([dicom.dataset.Dataset()])
+
+
+def test_load_dicom(dicom_path):
+    dicom_array = ld.load_dicom(dicom_path)
+
+    assert isinstance(dicom_array, np.ndarray)
+    with pytest.raises(errors.EmptyDicomSeriesException):
+        ld.load_dicom('./')
