3268dc55499e074b6bc7392566c1fa71b4c3222d
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2017-08-28 11:39:10 +0200

    Inspect a DICOM directory and create appropriate model instances (Closes: #39)

--- interface/backend/images/models.py
+++ interface/backend/images/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils._os import safe_join
+import dicom
+import glob
 
 
 class ImageSeries(models.Model):
@@ -10,6 +13,27 @@ class ImageSeries(models.Model):
     series_instance_uid = models.CharField(max_length=256)
 
     uri = models.CharField(max_length=512)
+
+    def get_or_create(uri):
+        """
+        Return the ImageSeries instance with the same PatientID and SeriesInstanceUID as the DICOM images in the
+        given directory. If none exists so far, create one.
+        Return a tuple of (ImageSeries, created), where created is a boolean specifying whether the object was created.
+
+        Args:
+            uri (str): absolute URI to a directory with DICOM images of a patient
+
+        Returns:
+            (ImageSeries, bool): the looked up ImageSeries instance and whether it had to be created
+        """
+        file_ = glob.glob1(uri, '*.dcm')[0]
+        plan = dicom.read_file(safe_join(uri, file_))
+        patient_id = plan.PatientID
+        series_instance_uid = plan.SeriesInstanceUID
+        return ImageSeries.objects.get_or_create(
+            patient_id=patient_id,
+            series_instance_uid=series_instance_uid,
+            uri=uri)
 
 
 class ImageLocation(models.Model):
@@ -23,3 +47,4 @@ class ImageLocation(models.Model):
     y = models.PositiveSmallIntegerField(help_text='Voxel index for Y axis, zero-index, from top left')
 
     z = models.PositiveSmallIntegerField(help_text='Slice index for Z axis, zero-index')
+
--- interface/backend/images/tests.py
+++ interface/backend/images/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from backend.images.factories import ImageSeriesFactory
+from backend.images.models import ImageSeries
 
 
 class SmokeTest(TestCase):
@@ -8,3 +9,17 @@ class SmokeTest(TestCase):
         image_series = ImageSeriesFactory()
         self.assertRegex(image_series.series_instance_uid, '^[0-9\.]{64}')
         self.assertIn(image_series.series_instance_uid, image_series.uri)
+
+    def test_get_create_image_series(self):
+        uri = '/images/LIDC-IDRI-0001/1.3.6.1.4.1.14519.5.2.1.6279.6001.298806137288633453246975630178/' \
+          '1.3.6.1.4.1.14519.5.2.1.6279.6001.179049373636438705059720603192'
+        image_series, created = ImageSeries.get_or_create(uri)
+        assert created
+        assert image_series.patient_id == 'LIDC-IDRI-0001'
+        assert image_series.series_instance_uid == '1.3.6.1.4.1.14519.5.2.1.6279.6001.179049373636438705059720603192'
+        assert image_series.uri == uri
+        image_series, created = ImageSeries.get_or_create(uri)
+        assert not created
+        assert image_series.patient_id == 'LIDC-IDRI-0001'
+        assert image_series.series_instance_uid == '1.3.6.1.4.1.14519.5.2.1.6279.6001.179049373636438705059720603192'
+        assert image_series.uri == uri
