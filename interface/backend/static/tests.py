59fc12548422ecaa861f810d0e40b631801e2de0
Isaac Slavitt <isaac.slavitt@gmail.com>
2017-09-04 15:49:04 -0400

    Fix failing test caused by view rename

--- interface/backend/static/tests.py
+++ interface/backend/static/tests.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class SmokeTest(TestCase):
     def test_landing(self):
-        url = reverse('static:home')
+        url = reverse('static:open_image')
         resp = self.client.get(url)
         self.assertContains(resp, 'Concept to Clinic')
         self.assertEqual(resp.status_code, 200)
