da9542d989c4be4c70bf714531bb5504373ce421
Isaac Slavitt <isaac.slavitt@gmail.com>
2017-09-11 17:10:15 -0400

    Don't check page content in backend test

--- interface/backend/static/tests.py
+++ interface/backend/static/tests.py
@@ -6,5 +6,4 @@ class SmokeTest(TestCase):
     def test_landing(self):
         url = reverse('static:home')
         resp = self.client.get(url)
-        self.assertContains(resp, 'Concept to Clinic')
         self.assertEqual(resp.status_code, 200)
