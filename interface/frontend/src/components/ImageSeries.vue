eb83532e419f777a81590af264b04b6914061eb9
Isaac Slavitt <isaac.slavitt@gmail.com>
2017-09-12 15:35:37 -0400

    Clean up ImageSeries.vue

--- interface/frontend/src/components/ImageSeries.vue
+++ interface/frontend/src/components/ImageSeries.vue
@@ -1,21 +1,24 @@
 <template>
-<div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="card">
-        <div class="card-block">
-          <h3 class="card-title">Available image series</h3>
-          <template v-if="availableSeries.length">
-            <ul>
-              <li v-for="series in availableSeries">
-                <a href="#" @click="selectSeries(series)">{{ series.series_instance_uid }}</a>
-                <span v-if="series == selected">&larr;</span>
-              </li>
-            </ul>
-          </template>
-        </div>
-      </div><!-- left side -->
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-block">
+            <h3 class="card-title">Available directories</h3>
+            <template v-if="directories.length">
+              <ul>
+                <li v-for="dir in directories">
+                  {{ dir }}
+                </li>
+              </ul>
+            </template>
+          </div>
+        </div><!-- left side -->
+      </div>
+
       <div class="col-md-8">
+
         <div class="row">
           <div class="col-md-12">
             <div class="card">
@@ -36,14 +39,14 @@
             </div>
           </div>
         </div>
+
         <div class="row" v-if="selected">
           <div class="col-md-12">
             <div class="card">
               <div class="card-block">
                 <h3 class="card-title">{{ selected.patient_id }}</h3>
-                <p class="card-text">
 
-                <table class="table table-bordered">
+                <table class="table table-bordered table-condensed">
                   <thead></thead>
                   <tr>
                     <th>key</th>
@@ -57,57 +60,57 @@
                   </tbody>
                 </table>
 
-                </p>
                 <a href="#" class="btn btn-primary">Start case</a>
               </div>
             </div>
           </div>
         </div>
+
       </div><!-- right side -->
-    </div>
-  </div>
-</div>
+
+    </div><!-- row -->
+  </div><!-- container -->
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      availableSeries: [],
-      directoriesDict: {},
-      selected: null
-    }
-  },
-  created () {
-    this.fetchData()
-    this.fetchAvailableImages()
-  },
-  methods: {
-    fetchData () {
-      this.$http.get('/api/images/').then(
-        (response) => {
-          this.availableSeries = response.body
-        },
-        () => {
-          // error callback
-        })
+  export default {
+    data () {
+      return {
+        availableSeries: [],
+        directories: [],
+        selected: null
+      }
     },
-    selectSeries (series) {
-      console.log(series.uri)
-      this.selected = series
+    created () {
+      this.fetchData()
+      this.fetchAvailableImages()
     },
-    fetchAvailableImages () {
-      this.$http.get('/images/available').then(
-        (response) => {
-          this.directoriesDict = response.body
-        },
-        () => {
-          // error callback
-        }
-      )
+    methods: {
+      fetchData () {
+        this.$http.get('/api/images/').then(
+          (response) => {
+            this.availableSeries = response.body
+          },
+          () => {
+            // error callback
+          })
+      },
+      selectSeries (series) {
+        console.log(series.uri)
+        this.selected = series
+      },
+      fetchAvailableImages () {
+        this.$http.get('/api/images/available').then(
+          (response) => {
+            this.directories = response.body.directories
+          },
+          () => {
+            // error callback
+          }
+        )
+      }
     }
   }
-}
 </script>
 
 <style>
