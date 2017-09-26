729dc1081fe6eaa20b0e11354deec78d84366af1
Isaac Slavitt <isaac.slavitt@gmail.com>
2017-09-26 16:16:14 -0400

    Fix image selection vue component to use available images payload

--- interface/frontend/src/components/ImageSeries.vue
+++ interface/frontend/src/components/ImageSeries.vue
@@ -1,114 +1,128 @@
 <template>
-<div class="container">
-  <div class="row">
-    <div class="col-md-4">
-      <div class="card">
-        <div class="card-block">
-          <h3 class="card-title">Available directories</h3>
-          <template v-if="directories.length">
-            <ul>
-              <li v-for="dir in directories">
-                {{ dir }}
-              </li>
-            </ul>
-          </template>
+  <div class="container" style="margin-top: 5em;">
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            Open imagery
+          </div>
+          <div class="card-block">
+            <template v-if="availableSeries.length">
+              <ul>
+                <li v-for="series in availableSeries">
+                  <a href="#" @click="selectSeries(series)">{{ series.series_instance_uid }}</a>
+                  <span v-if="series == selected">&larr;</span>
+                </li>
+              </ul>
+            </template>
+            <template v-else>
+              <p class="card-text">No images imported.</p>
+            </template>
+            <button class="btn btn-warning float-right"
+                    @click="showImport = !showImport"
+            >
+              Import
+            </button>
+          </div>
         </div>
-      </div><!-- left side -->
+      </div>
+    </div><!-- /row1 -->
+
+    <div class="row" v-show="showImport">
+      <div class="col-md-12">
+        <div class="card card-outline-warning">
+          <div class="card-header">
+            Import image series
+          </div>
+          <div class="card-block">
+            <tree-view class="item" :model="directories"></tree-view>
+          </div>
+        </div>
+      </div>
     </div>
 
-    <div class="col-md-8">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card" v-if="selected">
+          <div class="card-header">
+            Selected image series
+          </div>
+          <div class="card-block">
+            <h3 class="card-title">{{ selected.patient_id }}</h3>
 
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="card-block">
-              <h3 class="card-title">Available image series</h3>
-              <template v-if="availableSeries.length">
-                <ul>
-                  <li v-for="series in availableSeries">
-                    <a href="#" @click="selectSeries(series)">{{ series.series_instance_uid }}</a>
-                    <span v-if="series == selected">&larr;</span>
-                  </li>
-                </ul>
-              </template>
-              <template v-else>
-                <p class="card-text">No images available.</p>
-              </template>
-            </div>
+            <table class="table table-bordered table-condensed">
+              <thead>
+              <tr>
+                <th>key</th>
+                <th>value</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="(item, key, index) in selected">
+                <td>{{ key }}</td>
+                <td><small>{{ item }}</small></td>
+              </tr>
+              </tbody>
+            </table>
+
+            <a href="#" class="btn btn-primary float-right">Start case</a>
           </div>
         </div>
       </div>
+    </div><!-- /row2 -->
 
-      <div class="row" v-if="selected">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="card-block">
-              <h3 class="card-title">{{ selected.patient_id }}</h3>
-
-              <table class="table table-bordered table-condensed">
-                <thead></thead>
-                <tr>
-                  <th>key</th>
-                  <th>value</th>
-                </tr>
-                <tbody>
-                <tr v-for="(item, key) in selected">
-                  <td>{{ key }}</td>
-                  <td>{{ item }}</td>
-                </tr>
-                </tbody>
-              </table>
-
-              <a class="btn btn-primary">Start case</a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </div><!-- right side -->
-
-  </div><!-- row -->
-</div><!-- container -->
+  </div><!-- /container -->
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      availableSeries: [],
-      directories: [],
-      selected: null
-    }
-  },
-  created () {
-    this.fetchImages()
-    this.fetchDirectories()
-  },
-  methods: {
-    fetchImages () {
-      this.$axios.get('/api/images/').then(
-        (response) => {
-          this.availableSeries = response.data
-        },
-        () => {
-          // error callback
-        })
+  import TreeView from './TreeView'
+
+  export default {
+    components: {
+      TreeView
     },
-    selectSeries (series) {
-      this.selected = series
-    },
-    fetchDirectories () {
-      this.$axios.get('/api/images/available').then(
-        (response) => {
-          this.directories = response.data.directories
+    data () {
+      return {
+        availableSeries: [],
+        directories: {
+          name: 'root',
+          children: []
         },
-        () => {
-          // error callback
-        }
-      )
+        selected: null,
+        showImport: false
+      }
+    },
+    created () {
+      this.fetchData()
+      this.fetchAvailableImages()
+    },
+    methods: {
+      fetchData () {
+        this.$http.get('/api/images/').then(
+          (response) => {
+            this.availableSeries = response.body
+          },
+          () => {
+            // error callback
+          })
+      },
+      selectSeries (series) {
+        console.log(series.uri)
+        this.selected = series
+      },
+      fetchAvailableImages () {
+        this.$http.get('/api/images/available').then(
+          (response) => {
+            this.directories = response.body.directories
+          },
+          () => {
+            // error callback
+          }
+        )
+      }
     }
   }
-}
 </script>
 
 <style>
---
+++ interface/frontend/src/components/TreeView.vue
@@ -0,0 +1,92 @@
+<template>
+  <ul>
+    <li>
+      {{ model.name }}
+      <span v-if="isOpenable"  @click="toggle">
+        [{{open ? '-' : '+'}}]
+      </span>
+      <span class="badge badge-default" v-if="hasFiles">
+        {{ model.files.length }}
+      </span>
+      <ul v-show="isOpen" v-if="isFolder">
+        <tree-view class="item"
+                   v-for="child in model.children"
+                   v-if="child.children"
+                   :key="child.name"
+                   :model="child"
+        ></tree-view>
+        <li v-for="file in model.files" class="text-muted">{{ file.name }}</li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script>
+  import TreeView from './TreeView'
+
+  export default {
+    name: 'tree-view',
+    props: {
+      model: {
+        type: Object,
+        default: {
+          'name': 'root',
+          'children': [],
+          'files': [],
+          'type': 'folder'
+        }
+      }
+    },
+    components: {
+      TreeView
+    },
+    data () {
+      return {
+        open: false
+      }
+    },
+    computed: {
+      isFolder () {
+        return this.model.type === 'folder'
+      },
+      hasContents () {
+        return this.hasFiles || this.hasFolders
+      },
+      hasFolders () {
+        if (this.model.children) return this.model.children.length > 0
+        return false
+      },
+      hasFiles () {
+        if (this.model.files) return this.model.files.length > 0
+        return false
+      },
+      isOpenable () {
+        return this.isFolder && this.hasContents
+      },
+      isOpen () {
+        return this.open
+      }
+    },
+    methods: {
+      toggle: function () {
+        this.$set(this, 'open', !this.open)
+      }
+    }
+  }
+</script>
+
+<style>
+  .item {
+    cursor: pointer;
+  }
+
+  .bold {
+    font-weight: bold;
+  }
+
+  ul {
+    padding-left: 1em;
+    line-height: 1.5em;
+    list-style-type: dot;
+  }
+</style>
