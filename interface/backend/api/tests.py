87df6fcf9b54564c1610fa60788cbc9a56172fe0
Tahsin Hassan Rahit <tahsin.rahit@gmail.com>
2017-09-27 10:58:10 -0400

    Fix typo

--- interface/backend/api/tests.py
+++ interface/backend/api/tests.py
@@ -45,9 +45,9 @@ class ViewTest(TestCase):
     def test_images_metadata_view(self):
         url = reverse('images-metadata')
         response = self.client.get(url, {
-            'dicom_location': '"/images/LIDC-IDRI-0002/' \
+            'dicom_location': '/images/LIDC-IDRI-0002/' \
             '1.3.6.1.4.1.14519.5.2.1.6279.6001.490157381160200744295382098329/' \
-            '1.3.6.1.4.1.14519.5.2.1.6279.6001.619372068417051974713149104919/-80.750000.dcm"'
+            '1.3.6.1.4.1.14519.5.2.1.6279.6001.619372068417051974713149104919/-80.750000.dcm'
         })
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
