8c347412e413ad89954c2ad94aa4b7551bb0496c
Reuben Cummings <reubano@gmail.com>
2017-10-30 14:32:49 +0300

    Fix Config paths

--- prediction/config.py
+++ prediction/config.py
@@ -6,16 +6,19 @@
 """
 import os
 
+LIDC_WILDCARD = ['LIDC-IDRI-*', '**', '**']
+
 
 class Config(object):
     PROD_SERVER = os.getenv('PRODUCTION', False)
     DEBUG = False
     # The following paths are expanded at runtime
     CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+    PARENT_DIR = os.path.dirname(CURRENT_DIR)
     SEGMENT_ASSETS_DIR = os.path.abspath(os.path.join(CURRENT_DIR, 'src', 'algorithms', 'segment', 'assets'))
-    DICOM_PATHS_DOCKER_WILDCARD = os.path.join('/images_full', 'LIDC-IDRI-*', '**', '**')
-    DICOM_PATHS_LOCAL_WILDCARD = os.path.join(CURRENT_DIR, 'src', 'tests', 'assets',
-                                              'test_image_data', 'full', 'LIDC-IDRI-*', '**', '**')
+    DICOM_PATHS_DOCKER_WILDCARD = os.path.join(PARENT_DIR, 'images_full', *LIDC_WILDCARD)
+    DICOM_PATHS_LOCAL_WILDCARD = os.path.join(PARENT_DIR, 'tests', 'assets', 'test_image_data', 'full',
+                                              *LIDC_WILDCARD)
 
 
 class Production(Config):
