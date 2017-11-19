ecd080e47d13ff594f4919fd95fb41f39788e362
Peter Bull <pjbull@gmail.com>
2017-11-19 10:54:48 -0800

    Update commands with docker containers

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # use longform git hash as version
-VERSION=$(git show -s --format=%H)
+VERSION=$(git show -s --format=%h)
 
 pip install --user awscli # install aws cli w/o sudo
 pip install --user sceptre # install sceptre cli w/o sudo
@@ -18,14 +18,14 @@ docker tag concept-to-clinic/api:$VERSION 113913174193.dkr.ecr.us-east-1.amazona
 docker push 113913174193.dkr.ecr.us-east-1.amazonaws.com/c2c-api:$VERSION
 
 # build, tag, push  UI
-docker build -t concept-to-clinic/api:$VERSION --file ./compose/interface/Dockerfile-api .
-docker tag concept-to-clinic/api:$VERSION 113913174193.dkr.ecr.us-east-1.amazonaws.com/c2c-api:$VERSION
-docker push 113913174193.dkr.ecr.us-east-1.amazonaws.com/c2c-api:$VERSION
+docker build -t concept-to-clinic/ui:$VERSION --file ./compose/interface/Dockerfile-vue .
+docker tag concept-to-clinic/ui:$VERSION 113913174193.dkr.ecr.us-east-1.amazonaws.com/c2c-ui:$VERSION
+docker push 113913174193.dkr.ecr.us-east-1.amazonaws.com/c2c-ui:$VERSION
 
 # build, tag, push prediction
-docker build -t concept-to-clinic/api:$VERSION --file ./compose/interface/Dockerfile-api .
-docker tag concept-to-clinic/api:$VERSION 113913174193.dkr.ecr.us-east-1.amazonaws.com/c2c-api:$VERSION
-docker push 113913174193.dkr.ecr.us-east-1.amazonaws.com/c2c-api:$VERSION
+docker build -t concept-to-clinic/prediction:$VERSION --file ./compose/prediction/Dockerfile .
+docker tag concept-to-clinic/prediction:$VERSION 113913174193.dkr.ecr.us-east-1.amazonaws.com/c2c-prediction:$VERSION
+docker push 113913174193.dkr.ecr.us-east-1.amazonaws.com/c2c-prediction:$VERSION
 
 # deploy to AWS
 sceptre --dir 'deploy/' --var "APIVersion=$VERSION" --var "UIVersion=$VERSION" --var "PredictionVersion=$VERSION" \
