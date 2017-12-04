9ed7931bc6482b76b7e113755aea4a7d099ad823
Isaac Slavitt <isaac.slavitt@gmail.com>
2017-12-04 14:00:36 -0500

    Deal with HEAD possibly not existing

--- interface/frontend/config/dev.env.js
+++ interface/frontend/config/dev.env.js
@@ -1,9 +1,15 @@
 var merge = require('webpack-merge')
 var shell = require('shelljs')
 var prodEnv = require('./prod.env')
-var hash = shell.cat('HEAD').exec('cut -d " " -f2', {silent: true}).stdout
+
+var cat = shell.cat('HEAD')
+var hash = '0000000'
+if (cat.code === 0) {
+  var cut = cat.exec('cut -d " " -f2', {silent: true}).stdout
+  hash = JSON.stringify(cut.slice(0, 7))
+}
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  GIT_HASH_VERSION: JSON.stringify(hash.slice(0, 9))
+  GIT_HASH_VERSION: hash
 })
