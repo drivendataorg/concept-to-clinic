833c810aa22276d2e347065fb4cc98bbd7cf188b
Chris Lamb <chris@chris-lamb.co.uk>
2018-02-25 22:43:11 +0000

    Use `os.path.join` over constructing paths ourselves.

--- prediction/src/algorithms/identify/prediction.py
+++ prediction/src/algorithms/identify/prediction.py
@@ -25,7 +25,7 @@ CUBE_SIZE = 32
 CROP_SIZE = 32
 MEAN_PIXEL_VALUE = 41
 EXTRACTED_IMAGE_DIR = Config.EXTRACTED_IMAGE_DIR
-NODULE_DETECTION_DIR = "data/detections/"
+NODULE_DETECTION_DIR = os.path.join('data', 'detections')
 K.set_image_dim_ordering("tf")
 POS_WEIGHT = 2
 NEGS_PER_POS = 20
