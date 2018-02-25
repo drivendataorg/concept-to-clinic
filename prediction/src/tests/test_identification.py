e0e9fe48bad4358ecb3c60f2ea17e0fe23eaba98
Chris Lamb <chris@chris-lamb.co.uk>
2018-02-25 22:41:39 +0000

    Drop unnecessary parenthesis.

--- prediction/src/tests/test_identification.py
+++ prediction/src/tests/test_identification.py
@@ -9,7 +9,7 @@ def test_identify_nodules_001(dicom_path, nodule_001):
     predicted = trained_model.predict(dicom_path)
     first = predicted[0]
     dist = np.sqrt(np.sum([(first[s] - nodule_001[s]) ** 2 for s in ["x", "y", "z"]]))
-    assert (dist < 10)
+    assert dist < 10
 
 
 @pytest.mark.stop_timeout
@@ -17,7 +17,7 @@ def test_identify_nodules_003(dicom_path_003, nodule_003):
     predicted = trained_model.predict(dicom_path_003)
     first = predicted[0]
     dist = np.sqrt(np.sum([(first[s] - nodule_003[s]) ** 2 for s in ["x", "y", "z"]]))
-    assert (dist < 10)
+    assert dist < 10
 
 
 @pytest.mark.stop_timeout
@@ -25,4 +25,4 @@ def test_identify_luna(metaimage_path, luna_nodule):
     predicted = trained_model.predict(metaimage_path)
     first = predicted[0]
     dist = np.sqrt(np.sum([(first[s] - luna_nodule[s]) ** 2 for s in ["x", "y", "z"]]))
-    assert (dist < 10)
+    assert dist < 10
