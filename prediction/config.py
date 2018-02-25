5302cfe401a1f77fa878fd8ad1bc26dd2d77e224
Chris Lamb <chris@chris-lamb.co.uk>
2018-02-25 22:48:16 +0000

    Import os.path.{join,dirname,abspath} directly instead of importing `path` from os.path. This is more Pythonic.

--- prediction/config.py
+++ prediction/config.py
@@ -4,7 +4,7 @@ Provides the flask config options
 
 import os
 
-from os import path
+from os.path import abspath, dirname, join, realpath
 
 LIDC_WILDCARD = ('LIDC-IDRI-*', '**', '**')
 
@@ -12,18 +12,18 @@ LIDC_WILDCARD = ('LIDC-IDRI-*', '**', '**')
 class Config(object):
     PROD_SERVER = os.getenv('PRODUCTION', False)
     DEBUG = False
-    CURRENT_DIR = path.dirname(path.realpath(__file__))
-    PARENT_DIR = path.dirname(CURRENT_DIR)
-    ALGOS_DIR = path.abspath(path.join(CURRENT_DIR, 'src', 'algorithms'))
-    ALGO_CLASSIFY_GTR123_PATH = os.path.join(ALGOS_DIR, 'classify', 'assets', 'gtr123_model.ckpt')
-    SEGMENT_ASSETS_DIR = path.abspath(path.join(ALGOS_DIR, 'segment', 'assets'))
-    ALGO_SEGMENT_PATH = os.path.join(SEGMENT_ASSETS_DIR, 'best_model.hdf5')
-    FULL_DICOM_PATHS = path.join(PARENT_DIR, 'images_full')
-    SMALL_DICOM_PATHS = path.join(PARENT_DIR, 'images')
-    FULL_DICOM_PATHS_WILDCARD = path.join(FULL_DICOM_PATHS, *LIDC_WILDCARD)
-    SMALL_DICOM_PATHS_WILDCARD = path.join(FULL_DICOM_PATHS, *LIDC_WILDCARD)
-    DATA_DIR = path.abspath(path.join(CURRENT_DIR, 'data'))
-    EXTRACTED_IMAGE_DIR = path.abspath(path.join(CURRENT_DIR, 'extracted'))
+    CURRENT_DIR = dirname(realpath(__file__))
+    PARENT_DIR = dirname(CURRENT_DIR)
+    ALGOS_DIR = abspath(join(CURRENT_DIR, 'src', 'algorithms'))
+    ALGO_CLASSIFY_GTR123_PATH = join(ALGOS_DIR, 'classify', 'assets', 'gtr123_model.ckpt')
+    SEGMENT_ASSETS_DIR = abspath(join(ALGOS_DIR, 'segment', 'assets'))
+    ALGO_SEGMENT_PATH = join(SEGMENT_ASSETS_DIR, 'best_model.hdf5')
+    FULL_DICOM_PATHS = join(PARENT_DIR, 'images_full')
+    SMALL_DICOM_PATHS = join(PARENT_DIR, 'images')
+    FULL_DICOM_PATHS_WILDCARD = join(FULL_DICOM_PATHS, *LIDC_WILDCARD)
+    SMALL_DICOM_PATHS_WILDCARD = join(FULL_DICOM_PATHS, *LIDC_WILDCARD)
+    DATA_DIR = abspath(join(CURRENT_DIR, 'data'))
+    EXTRACTED_IMAGE_DIR = abspath(join(CURRENT_DIR, 'extracted'))
 
 
 class Production(Config):
